e9c409e57315e9d7eab7954510468e2e44c33f71
hode2002 <hovande.dev.2002@gmail.com>
2024-02-26 21:01:00 +0700

    remove matches validation

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, Matches } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
     @IsNotEmpty()
@@ -6,9 +6,6 @@ export class CreateUserDto {
     email: string;
 
     @IsNotEmpty()
-    @Matches(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?!.* ).{6,}$/, {
-        message:
-            'Password must contain one digit from 1 to 9, one lowercase letter, one uppercase letter and it must be 6 characters long.',
-    })
+    @IsString()
     password: string;
 }
