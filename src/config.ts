8747f65e9c04ef132158b7da6a0b2bcbe872d3cf
hode2002 <hovande.dev.2002@gmail.com>
2024-08-31 20:07:15 +0700

    update env config

--- src/config.ts
+++ src/config.ts
@@ -2,10 +2,16 @@ import { z } from 'zod';
 
 const configSchema = z.object({
     NEXT_PUBLIC_API_URL: z.string(),
+    NEXT_PUBLIC_GHN_URL: z.string(),
+    NEXT_PUBLIC_GHN_KEY: z.string(),
+    NEXT_PUBLIC_TINY_EDITOR_KEY: z.string(),
 });
 
 const configProject = configSchema.safeParse({
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_GHN_URL: process.env.NEXT_PUBLIC_GHN_URL,
+    NEXT_PUBLIC_GHN_KEY: process.env.NEXT_PUBLIC_GHN_KEY,
+    NEXT_PUBLIC_TINY_EDITOR_KEY: process.env.NEXT_PUBLIC_TINY_EDITOR_KEY,
 });
 
 if (!configProject.success) {
