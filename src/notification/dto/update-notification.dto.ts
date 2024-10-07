122410ba52de467bb39d633ebfd33242d7ea08f0
hode2002 <hovande.dev.2002@gmail.com>
2024-10-07 20:30:23 +0700

    add: notification type

--- src/notification/dto/update-notification.dto.ts
+++ src/notification/dto/update-notification.dto.ts
@@ -1,6 +1,8 @@
+import { PartialType } from '@nestjs/mapped-types';
 import { IsNumber, IsOptional } from 'class-validator';
+import { CreateNotificationDto } from './create-notification.dto';
 
-export class UpdateNotificationDto {
+export class UpdateNotificationDto extends PartialType(CreateNotificationDto) {
     @IsNumber()
     @IsOptional()
     status?: number;
--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -14,10 +14,65 @@ export class NotificationService {
 
     async create(createNotificationDto: CreateNotificationDto) {
         const slug = generateSlug(createNotificationDto.title);
-        return await this.prismaService.notification.create({
+        const users = await this.prismaService.user.findMany({
+            where: { is_active: true, role: 'USER' },
+        });
+
+        const notification = await this.prismaService.notification.create({
             data: {
                 ...createNotificationDto,
                 slug,
+            },
+        });
+
+        const notificationPromises = users.map((user) => {
+            return this.prismaService.userNotification.create({
+                data: {
+                    user_id: user.id,
+                    notification_id: notification.id,
+                },
+            });
+        });
+        await Promise.all(notificationPromises);
+
+        return notification;
+    }
+
+    async createUserNotification(createNotificationDto: CreateNotificationDto) {
+        if (!createNotificationDto?.user_id) {
+            throw new BadRequestException('Missing user id');
+        }
+
+        const { user_id, ...other } = createNotificationDto;
+
+        const slug = generateSlug(createNotificationDto.title);
+        const notification = await this.prismaService.notification.create({
+            data: {
+                ...other,
+                slug,
+            },
+        });
+
+        return await this.prismaService.userNotification.create({
+            data: {
+                user_id,
+                notification_id: notification.id,
+            },
+            include: {
+                notification: true,
+            },
+        });
+    }
+
+    async findAll() {
+        return await this.prismaService.notification.findMany({
+            where: {
+                type: {
+                    not: 'ORDER',
+                },
+            },
+            orderBy: {
+                created_at: 'desc',
             },
         });
     }
@@ -26,10 +81,14 @@ export class NotificationService {
         if (!userId) {
             throw new BadRequestException('Missing user id');
         }
-        return await this.prismaService.notification.findMany({
+        return await this.prismaService.userNotification.findMany({
             where: { user_id: userId },
             orderBy: {
                 created_at: 'desc',
+            },
+            select: {
+                status: true,
+                notification: true,
             },
         });
     }
@@ -60,11 +119,32 @@ export class NotificationService {
         return notify;
     }
 
-    async update(userId: string, updateNotificationDto: UpdateNotificationDto) {
-        const updated = await this.prismaService.notification.updateMany({
-            where: { user_id: userId },
-            data: { status: updateNotificationDto.status },
+    async update(
+        notificationId: string,
+        updateNotificationDto: UpdateNotificationDto,
+    ) {
+        const notification = await this.prismaService.notification.findUnique({
+            where: { id: notificationId },
         });
-        return updated;
+        if (!notification) {
+            throw new NotFoundException('Notification not found!');
+        }
+        return await this.prismaService.notification.update({
+            where: { id: notificationId },
+            data: { ...updateNotificationDto },
+        });
+    }
+
+    async userReadAllNotifications(userId: string) {
+        return await this.prismaService.userNotification.updateMany({
+            where: { user_id: userId },
+            data: { status: 1 },
+        });
+    }
+
+    async remove(id: string) {
+        return await this.prismaService.notification.delete({
+            where: { id },
+        });
     }
 }
