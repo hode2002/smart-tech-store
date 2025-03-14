0c5faf29919e4b1a6236206ffd86ae05352a320d
hode2002 <hovande.dev.2002@gmail.com>
2025-03-14 23:44:12 +0700

    fix: add missing line breaks in DTO files and reorder imports in review module

--- src/api/v1/modules/review/review.module.ts
+++ src/api/v1/modules/review/review.module.ts
@@ -1,9 +1,9 @@
 import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
-import { ProductService } from '@v1/modules/product/product.service';
 
 import { PrismaModule } from '@/prisma/prisma.module';
 import { MediaModule } from '@v1/modules/media/media.module';
+import { ProductService } from '@v1/modules/product/product.service';
 import { UserService } from '@v1/modules/user/user.service';
 
 import { ReviewController } from './review.controller';
