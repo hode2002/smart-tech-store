acc793c43f7ecc00bda7dc5b971c633016abdf47
hode2002 <hovande.dev.2002@gmail.com>
2025-03-20 21:05:07 +0700

    feat(cache): refactor CacheModule to support dynamic configuration with Redis store

--- src/api/v2/modules/cache/cache.module.ts
+++ src/api/v2/modules/cache/cache.module.ts
@@ -1,13 +1,42 @@
-import { CacheModule as NestCacheModule } from '@nestjs/cache-manager';
-import { Module } from '@nestjs/common';
+import {
+    CacheModuleOptions,
+    CacheStore,
+    CacheModule as NestCacheModule,
+} from '@nestjs/cache-manager';
+import { Module, DynamicModule } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { redisStore } from 'cache-manager-redis-store';
 
-import { cacheConfig } from '@/config/cache.config';
+import { CacheService } from '@v2/modules/cache/cache.service';
+@Module({})
+export class CacheModule {
+    static register<StoreConfig extends Record<any, any> = Record<string, any>>(
+        options: CacheModuleOptions<StoreConfig>,
+    ): DynamicModule {
+        return {
+            module: CacheModule,
+            imports: [
+                NestCacheModule.registerAsync({
+                    isGlobal: true,
+                    imports: [ConfigModule],
+                    useFactory: async (
+                        configService: ConfigService,
+                    ): Promise<CacheModuleOptions> => {
+                        const store = await redisStore({
+                            url: configService.get<string>('REDIS_URL'),
+                            ttl: options.ttl ?? configService.get<number>('CACHE_TTL', 3600),
+                        });
 
-import { CacheService } from './cache.service';
-
-@Module({
-    imports: [NestCacheModule.registerAsync(cacheConfig)],
-    providers: [CacheService],
-    exports: [CacheService],
-})
-export class CacheModule {}
+                        return {
+                            store: store as unknown as CacheStore,
+                            ttl: options.ttl ?? configService.get<number>('CACHE_TTL', 3600),
+                        };
+                    },
+                    inject: [ConfigService],
+                }),
+            ],
+            providers: [CacheService],
+            exports: [CacheService],
+        };
+    }
+}
