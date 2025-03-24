e507edbbb9473d4a71080f68c9cc8abf2274758c
hode2002 <hovande.dev.2002@gmail.com>
2025-03-25 00:05:22 +0700

    feat(user): add hamlet field to USER_WITH_ADDRESS_SELECT selector

--- src/prisma/selectors/users/user.selector.ts
+++ src/prisma/selectors/users/user.selector.ts
@@ -31,6 +31,7 @@ export const USER_WITH_ADDRESS_SELECT = {
             district: true,
             ward: true,
             address: true,
+            hamlet: true,
         },
     },
 } as const;
