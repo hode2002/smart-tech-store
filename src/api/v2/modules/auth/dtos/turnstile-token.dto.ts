c47fbfc262b35c999589ef2fde90bcddd70634b4
hode2002 <hovande.dev.2002@gmail.com>
2025-03-25 00:01:47 +0700

    feat(auth): enhance AuthController with Swagger documentation and update DTOs for user login and email verification

--- src/api/v2/modules/auth/dtos/turnstile-token.dto.ts
+++ src/api/v2/modules/auth/dtos/turnstile-token.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class TurnstileTokenDto {
+    @ApiProperty({ description: 'The Turnstile token for verification', example: 'abc123token' })
     @IsString()
     @IsNotEmpty()
     turnstileToken: string;
--- src/api/v2/modules/auth/dtos/user-login.dto.ts
+++ src/api/v2/modules/auth/dtos/user-login.dto.ts
@@ -1,14 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
-export class VerifyOtpDto {
+export class UserLoginDto {
     @ApiProperty({ description: 'The email of the user', example: 'user@example.com' })
-    @IsEmail()
     @IsNotEmpty()
+    @IsEmail()
     email: string;
 
-    @ApiProperty({ description: 'The OTP code sent to the user', example: '123456' })
+    @ApiProperty({ description: 'The password of the user', example: 'pass123' })
     @IsNotEmpty()
     @IsString()
-    otpCode: string;
+    password: string;
 }
