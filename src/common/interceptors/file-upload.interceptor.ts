7970a4899deebdd99b16999c2ea67e95fd2f752b
hode2002 <hovande.dev.2002@gmail.com>
2025-03-24 17:39:53 +0700

    feat(interceptors): enhance FileUploadInterceptor to support multiple file uploads and custom file size limits

--- src/common/interceptors/file-upload.interceptor.ts
+++ src/common/interceptors/file-upload.interceptor.ts
@@ -1,12 +1,38 @@
 import { Injectable, UnsupportedMediaTypeException } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 
 @Injectable()
 export class FileUploadInterceptor {
-    static CustomField(fieldName: string) {
+    static CustomField(fieldName: string, maxFiles = 10) {
         const allowedMimeTypes = process.env.ALLOWED_MIME_TYPES?.split(',') || [
             'image/png',
             'image/jpeg',
+            'video/mp4',
+        ];
+
+        const maxSizeMB = Number(process.env.MAX_FILE_SIZE_MB) || 5;
+
+        return FilesInterceptor(fieldName, maxFiles, {
+            limits: { fileSize: maxSizeMB * 1024 * 1024 },
+            fileFilter: (req, file, cb) => {
+                if (!allowedMimeTypes.includes(file.mimetype)) {
+                    return cb(
+                        new UnsupportedMediaTypeException(
+                            `Only ${allowedMimeTypes.join(', ')} files are allowed!`,
+                        ),
+                        false,
+                    );
+                }
+                cb(null, true);
+            },
+        });
+    }
+
+    static CustomSingleField(fieldName: string) {
+        const allowedMimeTypes = process.env.ALLOWED_MIME_TYPES?.split(',') || [
+            'image/png',
+            'image/jpeg',
+            'video/mp4',
         ];
 
         const maxSizeMB = Number(process.env.MAX_FILE_SIZE_MB) || 5;
